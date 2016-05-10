1755c6daaf351c14b9784d4d48f0ae34b53c136d
David <David@a7d008b5-31d7-441d-85c2-377991e666be>
2016-05-10 00:48:54 +0000

    Date Range Filter

--- src/bettingManager/gui/MainController.java
+++ src/bettingManager/gui/MainController.java
@@ -5,6 +5,16 @@ import javafx.fxml.FXML;
 public class MainController {
 
 	
+	/**
+	 * -- Filter order --
+	 * 1 Checkbox1Controller: Historic Real
+	 * 2 OptionsController: Date Range, Tipsters, More Filters, Clear All, Refresh
+	 * 3 SiteController: BetAdvisor,..
+	 * 4 AverageOddsController: Greater than,...
+	 * 5 KoBController: Asian Handicap,..
+	 * 6 Liquidity: Greater than,...
+	 */
+	
 	@FXML 
 	public void initialize() {
 
--- src/bettingManager/gui/OptionsController.java
+++ src/bettingManager/gui/OptionsController.java
@@ -1,8 +1,15 @@
 package bettingManager.gui;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 /**
  * 2nd item of filters
@@ -25,8 +32,13 @@ public class OptionsController {
 		this.mainC = mainC;
 	}
 	
-	public void handleDateRangeButton(ActionEvent event) {
+	public void handleDateRangeButton(ActionEvent event) throws IOException {
 		System.out.println("date range button clicked");
+		
+		//TODO: Maybe use MenuItem instead of entire new window for better visuals
+		Stage dateRangePopup = new Stage();
+		dateRangePopup.setScene(new Scene(FXMLLoader.load(getClass().getResource("/bettingManager/gui/layout/Options_DateRange.fxml"))));
+		dateRangePopup.show();
 	}
 
 	public void handleTipstersButton(ActionEvent event) {
