de76e2d6052aea80df46f621350573330fd50192
David <David@a7d008b5-31d7-441d-85c2-377991e666be>
2016-05-13 15:58:57 +0000

    KoB Controller

--- src/bettingManager/gui/KoBController.java
+++ src/bettingManager/gui/KoBController.java
@@ -1,16 +1,37 @@
 package bettingManager.gui;
 
+import java.util.ArrayList;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
+import javafx.scene.Node;
+import javafx.scene.control.CheckBox;
 import javafx.scene.layout.GridPane;
 
 public class KoBController {
 	private MainController mainC;
-	
+	private ArrayList<String> checkedKoB;
 	
 	@FXML GridPane koBGrid;
 	
 	public void init(MainController mainC) {
 		this.mainC = mainC;
 	}
+	
+	@FXML
+	public void handleKoBCheckbox(ActionEvent event) {
+		System.out.println("CHECKBOX SITE");
+		checkedKoB = new ArrayList<String>();
+		for (Node cb : koBGrid.getChildren()) {
+			if (((CheckBox)cb).isSelected()) {
+				checkedKoB.add( ((CheckBox)cb).getText());
+			}
+		}
+		System.out.println(checkedKoB);
+	}
+	
+	public ArrayList<String> getCheckedSites() {
+		return checkedKoB;
+	}
+	
 }
