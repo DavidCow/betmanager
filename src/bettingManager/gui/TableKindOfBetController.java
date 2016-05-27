3c575c08ea30bf31bcd7fc923f847f3498f63c03
David <David@a7d008b5-31d7-441d-85c2-377991e666be>
2016-05-27 12:55:56 +0000

    Multi selection of rows

--- src/bettingManager/gui/TableKindOfBetController.java
+++ src/bettingManager/gui/TableKindOfBetController.java
@@ -5,8 +5,10 @@ import java.util.List;
 import java.util.Observable;
 
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -15,23 +17,24 @@ public class TableKindOfBetController extends Observable{
 	private MainController mainC;
 	public static int OPTIONS_TABLEKINDOFBET_ID = 10;
 	
-	  private final ObservableList<String> data =
-		        FXCollections.observableArrayList(
-		        		"T", "T2", "T3","T", "T2", "T3","T", "T2", "T3"
-		        		);   
-	
 	@FXML TableView<TableValue> tableKindOfBet;
 	
 	/**
 	 * Initialize
-	 * @param mainC
-	 * @param opt
 	 */
 	public void init(MainController mainC) {
 		this.mainC = mainC;
 		inflateTable(TableTitles.TABLE_TITLES_KINDOFBET);
+
+		tableKindOfBet.getSelectionModel().setSelectionMode(
+		    SelectionMode.MULTIPLE
+		);
 	}
 	
+	/**
+	 * Populate Table with Column Headers
+	 * @param tableTitles
+	 */
 	private void inflateTable(String [] tableTitles) {
 		for(int i = 0; i<tableTitles.length; i+=1) {
 			TableColumn<TableValue, String> newTC = new TableColumn<TableValue, String>(tableTitles[i]);
@@ -62,12 +65,32 @@ public class TableKindOfBetController extends Observable{
 		tv.setAverageLiquidity(new String("works"));
 		tv.setPercentOfTipsFound(new String("works"));
 		tv.setFlatStakeYield(new String("works"));
+		
+		TableValue tv2 = new TableValue();
+		tv2.setKindOfBet(new String("works2"));
+		tv2.setAverageYield(new String("works2"));
+		tv2.setAverageOdds(new String("works2"));
+		tv2.setNumberOfBets(new String("works2"));
+		tv2.setPercentWeGet(new String("works2"));
+		tv2.setPercentOver95(new String("works2"));
+		tv2.setAverageLiquidity(new String("works2"));
+		tv2.setPercentOfTipsFound(new String("works2"));
+		tv2.setFlatStakeYield(new String("works2"));
+		
 		List<TableValue> arr = new ArrayList<TableValue>();
 		arr.add(tv);
+		arr.add(tv2);
 		ObservableList<TableValue> data = FXCollections.observableList(arr);
 		
 		tableKindOfBet.getItems().setAll(data);
-		
+		tableKindOfBet.getSelectionModel().getSelectedItems().addListener(new ListChangeListener<TableValue>() {
+
+			@Override
+			public void onChanged(ListChangeListener.Change<? extends TableValue> c) {
+			     for( TableValue t : c.getList())
+		                System.out.println(t);
+			}
+		});
 		/**
 		 * TEST TO FILL IN TABLES WITH DATA
 		 */
