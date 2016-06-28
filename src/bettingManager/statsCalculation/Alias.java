a557580417d0a24ce864673ea58c550aa5863564
David <David@a7d008b5-31d7-441d-85c2-377991e666be>
2016-06-28 10:21:26 +0000

    alias

---
+++ src/bettingManager/statsCalculation/Alias.java
@@ -0,0 +1,19 @@
+package bettingManager.statsCalculation;
+
+import java.util.ArrayList;
+
+public class Alias extends ArrayList<String> {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String aliasName = "DefaultAliasName";
+	public String getAliasName() {
+		return aliasName;
+	}
+	public void setAliasName(String aliasName) {
+		this.aliasName = aliasName;
+	}
+	
+	
+	
+}
